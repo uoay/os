b7d36162e5f42d316832cfe6acddc65b8a6cce71
uoay <67417191+uoay@users.noreply.github.com>
2024-05-04 17:30:33 +0800

    fix(kernel): clear bss section

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,20 @@ global_asm!(include_str!("entry.asm"));
 
 #[no_mangle]
 fn rust_main() -> ! {
+    clear_bss();
     println!("Hello World!");
     loop {}
 }
+
+fn clear_bss() {
+    extern "C" {
+        fn sbss();
+        fn ebss();
+    }
+    (sbss as usize..ebss as usize).for_each(|a| {
+        unsafe {
+            (a as *mut u8).write_volatile(0)
+        }
+    });
+}
+
